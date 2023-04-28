85860146ee0500919af33b25e6efded6a1f3b881
Alper Demirci <alprdmrc20@gmail.com>
2023-04-28 14:21:31 +0300

    initial tables created

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // serverı buraya yazın ve index.js require yazın
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("*", (req, res, next) => {
+  res.send("<h1>WELCOME TO SPRINT 14 CHALLENGE<h1>");
+});
+
+module.exports = server;
---
+++ data/migrations/20230428105051_create-initial-tables.js
@@ -0,0 +1,63 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name", 64).notNullable();
+      tbl.string("project_description", 128);
+      tbl.boolean("project_completed").notNullable().defaultTo(false);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.string("resource_name", 64).notNullable().unique();
+      tbl.string("resource_description");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.string("task_description", 128).notNullable();
+      tbl.string("task_notes", 64);
+      tbl.boolean("task_completed").notNullable().defaultTo(false);
+      tbl
+        .bigInteger("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl
+        .bigInteger("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .bigInteger("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resource_id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.primary(["project_id", "resource_id"]);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
